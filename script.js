d28ed91b84e4c4456c71ed007f79e2c93f6aaa56
LungeloSarao <lzondi@sarao.ac.za>
2026-07-09 16:33:28 +0200

    Added multiplication

--- script.js
+++ script.js
@@ -1,5 +1,9 @@
-function add(a, b) {
-    return a + b;
+function add(a,b){
+    return a+b;
 }
 
-console.log(add(5, 3));
+function multiply(a,b){
+    return a*b;
+}
+
+console.log(multiply(5,4));
